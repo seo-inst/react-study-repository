e897bcd5b3f8ac427c53ddad93bc9ac17f21806e
seo-inst <innerpeace.deepi@gmail.com>
2025-09-10 15:42:02 +0900

    feat:react lesson2,3 done

--- lesson3-react-app-pros/src/App.jsx
+++ lesson3-react-app-pros/src/App.jsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import Card from "./components/Card";
 /*
   React Props : 부모 컴포넌트가 자식 컴포넌트에게 데이터를 전달하는 기술 
                 자식 컴포넌트에서는 읽기 전용으로만 사용가능 
@@ -7,6 +8,7 @@ import "./App.css";
   - 게시판 글 목록 등에서 사용될 수 있음               
 */
 function App() {
+  // jsx : 화면 렌더링될 정보를 리턴
   return (
     <div className="app">
       <header className="app-header">
@@ -14,7 +16,53 @@ function App() {
         <p>Props를 활용한 재사용 가능한 Card 컴포넌트</p>
       </header>
       <div className="card-grid">
-
+        {/* jsx 주석 */}
+        <Card
+          category="React"
+          title="컴포넌트와 Props"
+          description="React 핵심 개념인 컴포넌트와 Prop를 학습"
+          author="손흥민"
+          date="2025-9-10"
+          image="https://picsum.photos/400/300?random=1"
+        />
+        <Card
+          category="Javascript"
+          title="ES6+ 문법정리"
+          description="화살표함수,구조분해할당,스프레드연산자 등"
+          author="조규성"
+          date="2025-9-10"
+          image="https://picsum.photos/400/300?random=2"
+        />
+        {/* 
+            선택적 Props 전달 
+            - author props 를 전달하지 않음 
+            - Card 컴포넌트에서 전달되지 않은 값을 처리 ( 존재할때만 렌더링)
+        */}
+        <Card
+          category="CSS"
+          title="CSS와 React Bootstrap"
+          description="React Bootstrap Grid system으로 반응형 웹을 구현"
+          date="2025-9-10"
+          image="https://picsum.photos/400/300?random=3"
+        />
+        {/* 
+          author 와 date 를 전달하지 않고 Card 컴포넌트 jsx 조건식 테스트 
+        */}
+        <Card
+          category="React와 state"
+          title="React state로 내부 상태 변화 반영"
+          description="React state hooks로 내부 상태 변경시 렌더링하도록 구현"          
+          image="https://picsum.photos/400/300?random=4"
+        />
+        {/* 
+            최소한의 정보만 전달, 이번에는 category 도 전달하지 않아본다 
+            Card Component 에서 category 정보가 없으면 일반이라고 조건문을 이용해 처리하도록 하고 테스트 
+        */}
+        <Card 
+            title="JPA 학습"
+            description="N+1 문제 해결을 위한 JPQL Fetch Join을 학습합시다"
+            image="https://picsum.photos/400/300?random=5"
+        />
       </div>
     </div>
   );
--- lesson3-react-app-pros/src/components/Card.jsx
+++ lesson3-react-app-pros/src/components/Card.jsx
@@ -1,29 +1,30 @@
-import "./Card.css"
+import "./Card.css";
 /*
     부모 컴포넌트인 App.jsx 에서 전달하는 Pros 정보를 받는 자식 Card 컴포넌트 
     방식 :  function 컴포넌트(props){
                 //pros.속성명으로 접근 
             }
 */
-function Card(props){
-    return (
-      <div className="card-container">
-      <span className="card-category">
-       
-      </span>
-      <img     
-        className="card-image"
-      />
+function Card(props) {
+  return (
+    <div className="card-container">
+    <span className="card-category">
+    {/* props.category가 있으면 그 값 사용, 없으면(undefined,null,빈문자열) "일반" 사용 */}
+        {props.category || "일반" }
+    </span>
+      <img className="card-image" src={props.image} alt={props.title} />
       <div className="card-content">
-        <h3 className="card-title"></h3>
+        <h3 className="card-title">{props.title}</h3>
+        <p className="card-description">{props.description}</p>
+        {/* 
+            선택적 Props - 조건부 렌더링 (&& 연산자)
+            props.author가 존재할 때만 렌더링 
+        */}
+        {props.author && <p className="card-meta">작성자: {props.author}</p>}
+        {props.date && <p className="card-meta">작성일: {props.date}</p>}    
         
-        <p className="card-description"></p>        
-        
-        <p className="card-meta"></p>
-       
-        <p className="card-meta"></p>       
       </div>
     </div>
-    )
+  );
 }
 export default Card;
