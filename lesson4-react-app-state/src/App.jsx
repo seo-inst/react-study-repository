4cd96d0a90fa95428d22167cc7fc6da25ff901c3
seo-inst <innerpeace.deepi@gmail.com>
2025-09-11 15:57:33 +0900

    feat:state hook

---
+++ lesson4-react-app-state/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Counter from "./components/Counter";
+
+function App(){
+
+  return (
+    <div className="app">
+      <h1>useState Hook</h1>
+      <Counter />
+    </div>
+  )
+}
+export default App;
---
+++ lesson4-react-app-state2-likebutton/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import LikeButton from "./components/LikeButton";
+
+function App() {
+  return (
+    <div className="app">
+      <h1>React useState Hook & Event Handling</h1>
+      <LikeButton/>
+    </div>
+  );
+}
+export default App;
